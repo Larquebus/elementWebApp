f93e0c2c7e894c739ed8ceb5c839792dd0f2fe5f
Chris <quill9@gmail.com>
2018-10-19 19:27:50 -0500

    Commit initial files to master.

---
+++ elementWebApp.py
@@ -0,0 +1,118 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.properties import ObjectProperty, StringProperty
+
+from elementWebData import *
+
+# The core of the application. When initialized it taps into the web data file:
+class AppFrame(BoxLayout):
+  web_data = ObjectProperty(webData())
+  selected_element = ObjectProperty(None)
+  focus_object = ObjectProperty(None)
+  element_display = ObjectProperty(None)
+  element_details = ObjectProperty(None)
+  
+  # Updates values for whatever element is currently selected, saves the element back to the web:
+  def updateElement(self, value_to_update, new_value):
+    
+    # These lines are repetitive... clean up somehow?
+    self.selected_element.element_dict[value_to_update] = new_value
+    self.web_data.elements[self.selected_element.element_key].element_dict[value_to_update] = new_value
+    self.web_data.elements[self.selected_element.element_key].resynchronize()
+    self.web_data.save()
+
+"""
+These widgets make up the left hand window of the application, which contains all widgets for the 
+Element Web itself.
+"""
+# Holds the various ways of displaying Elements and the ElementDisplayBar:
+class ElementDisplay(BoxLayout):
+  pass
+  
+# A bar of display formats for Elements in the Web. Sits at the top of ElementDisplay:
+class ElementDisplayBar(BoxLayout):
+  pass
+  
+# A window of display formats resulting from ElementDisplayBar selections:
+class ElementDisplayWindow(Widget):
+  pass
+  
+# Tab that displays all Elements in the Web on one screen:
+class ElementFlatScroll(ScrollView):
+  pass
+  
+class ElementFlat(StackLayout):
+  web_to_display = ObjectProperty(None)
+  root_link = ObjectProperty(None)
+  
+  # Loops through all Elements in the Web and adds them to the ScrollView:
+  def getFlatElements(self):
+    self.clear_widgets()
+    counter = 0
+#	new_elements = self.web_to_display.elements
+    for e_id in self.web_to_display.elements:
+      new_element = self.web_to_display.elements[e_id]
+      self.add_widget(Element(id=e_id, 
+                              element_data=new_element, 
+                              element_name=new_element.name
+                              )
+                      )
+      counter += 1
+    print("Added %i elements to flat element display." % counter)
+  
+# Tab that displays a focus Element and its related Elements from the Web:
+class ElementWeb(BoxLayout):
+  focus_name = StringProperty()
+
+# Used to block out the sub-regions of the ElementWeb window:
+class WebRegion(BoxLayout):
+  pass
+  
+# All visible widgets in the ElementWeb are Elements of various types.
+class Element(Button):
+  element_key = StringProperty()
+  element_data = ObjectProperty(None)
+  element_name = StringProperty()
+  def selectElement(self):
+    self.parent.root_link.selected_element = self.element_data
+    print('Selected ' + self.element_data.name)
+    self.parent.root_link.element_details.activateElementNotes()
+
+"""
+These widgets make up the right hand window of the application, which shows notes and other details 
+about elements selected in the Element Web window.
+"""
+class ElementDetails(BoxLayout):
+  root_link = ObjectProperty(None)
+  element_notes = ObjectProperty(None)
+  def activateElementNotes(self):
+    self.clear_widgets()
+    self.element_notes = ElementNotes()
+    self.add_widget(self.element_notes)
+    self.setNotesText()
+  def setNotesText(self):
+    self.element_notes.text = self.root_link.selected_element.notes
+
+class ElementNotes(TextInput):
+  pass
+  
+"""
+Build the app:
+"""
+class elementWebApp(App):
+  def build(self):
+    app = AppFrame()
+#    app.focus_name = app_data.web_data.elements["e" + str(app_data.web_data.meta_data["last_id"])].name
+    print(app.web_data.meta_data)
+#    print(app_data.focus_name)
+	
+    return app
+
+
+if __name__ == '__main__':
+  elementWebApp().run()
---
+++ elementWebData.py
@@ -0,0 +1,68 @@
+"""
+  ======================================================================================================
+  These functions create and control all the data for the elements of the web 
+  and their relationships to each other.
+  ======================================================================================================
+"""
+
+import json
+
+"""
+The elementData object is the basic building block of the entire web.
+"""
+class elementData:
+  def __init__(self, dict):
+    self.element_dict = dict
+    self.id = dict["id"]
+    self.element_key = "e" + str(self.id)
+    self.type = self.element_dict["type"]
+    self.name = self.element_dict["name"]
+    self.notes = self.element_dict["notes"]
+	
+#    if (self.type == 'player' or self.type == 'npc'):
+#     self.allies = dict["ally_ids"]
+#      self.enemies = dict["enemy_ids"]
+	
+  def resynchronize(self):
+    self.type = self.element_dict["type"]
+    self.name = self.element_dict["name"]
+    self.notes = self.element_dict["notes"]    
+
+"""
+The webData object essentially acts a database of elementData objects.
+"""
+class webData:
+  def __init__(self):
+    self.file_path = 'web.json' # This could be user selected someday?
+    self.web_data_list = []
+    self.meta_data = None
+    self.id_list = []
+    self.elements = {}
+    # Add a try/catch here to account for corrupted save files?
+    # Load the selected file:
+    with open(self.file_path, 'r') as r:
+      for i, line in enumerate(r):
+        # The first line of the save file should contain meta data needed to set up the web UI.
+        if i == 0:
+          self.meta_data = json.loads(line)
+        # The remaining lines should contain data for each element in the web.
+        else:
+          temp_dict = json.loads(line)
+          self.web_data_list.append(temp_dict)
+		  
+    # Populate the id_list with each id from the loaded dictionaries, and create 
+	# an elementData object for each id:
+    for i in self.web_data_list:
+      id = i["id"]
+      self.id_list.append(id)
+      self.elements["e" + str(id)] = elementData(i)
+	  
+  # This method saves the web's list of meta data and dictionaries to the selected file.
+  def save(self):
+    with open(self.file_path, 'w') as w:
+      w.write(json.dumps(self.meta_data) + '\r')
+    with open(self.file_path, 'a') as a:
+      for i in self.web_data_list:
+        a.write(json.dumps(i) + '\r')
+  
+
